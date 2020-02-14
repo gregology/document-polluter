f384d9037f0417ba91a14d37a1dccd4216167b50
Greg Clarke <greg.clarke@shopify.com>
2020-02-14 17:28:04 -0500

    adding more words

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='document-polluter',
-    version='0.0.4',
+    version='0.0.5',
     description='Pollutes documents with terms biased on specific geners',
     long_description=long_description,
     long_description_content_type='text/markdown',
